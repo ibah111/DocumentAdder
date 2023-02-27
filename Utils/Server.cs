a78cd1ac4446f0649747606bda3bb7d726533012
Alex Dev Sm <leshasmlesha@gmail.com>
2023-02-27 17:24:58 +0300

    Добавил зацикливание

--- Utils/Server.cs
+++ Utils/Server.cs
@@ -7,6 +7,7 @@ namespace DocumentAdder.Utils
         HttpListener listener;
         public delegate void CallGetToken(string token);
         public event CallGetToken OnGetToken;
+        private bool started;
         public Server()
         {
             listener = new HttpListener();
@@ -14,20 +15,22 @@ namespace DocumentAdder.Utils
         }
         public void Start()
         {
+            started = true;
             listener.Start();
+            while(started)
             handler();
         }
         public void handler()
         {
             var context = listener.GetContext();
             var request = context.Request;
+            var response = context.Response;
             if (request.HttpMethod == "GET" && request.Url.AbsolutePath == "/login")
             {
                 var token = request.QueryString.Get("token");
                 if (token != null)
                 {
                     OnGetToken(token);
-                    var response = context.Response;
                     string responseString = $"<HTML><BODY>Вы авторизованы. Вы можете закрыть эту страницу</BODY></HTML>";
                     byte[] buffer = System.Text.Encoding.GetEncoding("windows-1251").GetBytes(responseString);
                     response.ContentLength64 = buffer.Length;
@@ -35,13 +38,14 @@ namespace DocumentAdder.Utils
                     output.Write(buffer, 0, buffer.Length);
                     // You must close the output stream.
                     output.Close();
+                    started = false;
                     listener.Stop();
                     OnGetToken(token);
                 }
-                else
-                {
-                    listener.Stop();
-                }
+            }
+            else {
+                response.AppendHeader("Access-Control-Allow-Origin", "*");
+                response.Close();
             }
         }
     }
