3e1503e6ea4cc679414e1851dfa3be5166a91610
Alex Dev Sm <leshasmlesha@gmail.com>
2023-06-30 14:39:35 +0300

    Тоже пофиксил отображение

--- Utils/Searcher.cs
+++ Utils/Searcher.cs
@@ -65,7 +65,7 @@ namespace DocumentAdder.Utils
                 " left join users u on u.id = wt.r_user_id " +
                 $" where {where1} and le.state != 5 and le.state != 6";*/
             Settings.command1 = "select TOP(100) le.id,(select name from dict where parent_id = 77 and code = le.state) Status,(select name from dict where parent_id = 6 and code=d.status) Debt_status, " +
-                "u.f+' '+u.i+' '+u.o fio_vz,pr.name,p.f+' '+p.i+' '+p.o,p.birth_date,le.contract,le.fssp_doc_num , le.court_doc_num,lc.name,convert(varchar,la.total_sum) + ' Руб',lc1.name,la.exec_number, " +
+                "u.f+' '+u.i+' '+u.o fio_vz,pr.name,p.f+' '+p.i+' '+p.o,p.birth_date,d.contract,le.fssp_doc_num , le.court_doc_num,lc.name,convert(varchar,la.total_sum) + ' Руб',lc1.name,la.exec_number, " +
                 "le.dsc, CONVERT(varchar, le.court_date, 104) court_date1,lc1.address court_adress1,cast(d.start_sum as decimal(16,2)) start_sum1, pass.number number1,pass.series series1,p.birth_place birth_place1, " +
                 "a.full_adr full_adr1, la.court_order_date exec_date1, p.inn inn1 from law_exec le " +
                 "left join person p on p.id = le.r_person_id " +
