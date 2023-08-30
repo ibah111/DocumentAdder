7b5a59bf15992f40bcf509db3f3337877602fcaf
Alex Dev Sm <leshasmlesha@gmail.com>
2023-08-30 09:28:02 +0300

    feat(models): добавил модели

---
+++ DatabaseContact/DatabaseContact/Configurations/DictConfiguration.cs
@@ -0,0 +1,46 @@
+﻿
+using DatabaseContact.DatabaseContact;
+using DatabaseContact.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+
+
+namespace DatabaseContact.DatabaseContact.Configurations
+{
+    public partial class DictConfiguration : IEntityTypeConfiguration<Dict>
+    {
+        public void Configure(EntityTypeBuilder<Dict> entity)
+        {
+            entity.ToTable("dict", tb =>
+            {
+                tb.HasComment("словарные термины");
+                tb.HasTrigger("dict_change");
+                tb.HasTrigger("tr_dict_bidu");
+            });
+
+            entity.HasIndex(e => new { e.parent_id, e.lang_canonical, e.code }, "UC_DICT_ID")
+            .IsUnique()
+            .HasFillFactor(100);
+
+            entity.Property(e => e.EXT_ID).HasComment("Внешний код");
+            entity.Property(e => e.LANG).HasComment("язык (словарь 78)");
+            entity.Property(e => e.change_dt).HasComment("дата изменения записи");
+            entity.Property(e => e.code).HasComment("код термина");
+            entity.Property(e => e.lang_canonical).HasComputedColumnSql("(coalesce([lang],(1)))", false);
+            entity.Property(e => e.parent_id).HasComment("ссылка на code описателя слова");
+            entity.Property(e => e.r_code).HasComment("ссылка на термин родительского");
+            entity.Property(e => e.typ).HasComment("1 - системный, 2 - общий");
+
+            entity.HasOne(d => d.parent).WithMany(p => p.dict)
+            .OnDelete(DeleteBehavior.ClientSetNull)
+            .HasConstraintName("FK_dict_parent_id_dict_name");
+
+            OnConfigurePartial(entity);
+        }
+
+        partial void OnConfigurePartial(EntityTypeBuilder<Dict> entity);
+    }
+
+}
---
+++ DatabaseContact/DatabaseContact/Configurations/DictNameConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using DatabaseContact.DatabaseContact;
+using DatabaseContact.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+
+
+namespace DatabaseContact.DatabaseContact.Configurations
+{
+    public partial class DictNameConfiguration : IEntityTypeConfiguration<DictName>
+    {
+        public void Configure(EntityTypeBuilder<DictName> entity)
+        {
+            entity.ToTable("dict_name", tb =>
+            {
+                tb.HasComment("описатель словаря");
+                tb.HasTrigger("tr_dict_name_bidu");
+            });
+
+            entity.Property(e => e.code)
+            .ValueGeneratedNever()
+            .HasComment("номер словаря");
+            entity.Property(e => e.NAME2).HasComment("название на альтернативном языке");
+            entity.Property(e => e.NOT_USE).HasComment("признак отсутствия использования словаря у Клиента. 1 – словарь не используется. Иначе – используется");
+            entity.Property(e => e.id).ValueGeneratedOnAdd();
+            entity.Property(e => e.r_code).HasComment("код родительского словаря");
+            entity.Property(e => e.typ).HasComment("1 - системный, 2 - общий");
+
+            OnConfigurePartial(entity);
+        }
+
+        partial void OnConfigurePartial(EntityTypeBuilder<DictName> entity);
+    }
+}
