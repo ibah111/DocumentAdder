beaf7fb585b29570f10d8534fbef07d32c4ff38f
Alex Dev Sm <leshasmlesha@gmail.com>
2023-08-30 15:48:17 +0300

    feat(search): дописал поиск

--- Utils/LawExecResult.cs
+++ Utils/LawExecResult.cs
@@ -4,32 +4,31 @@
 
 namespace DocumentAdder.Utils;
 
-public class LawActResult
+public class LawExecResult
 {
     public int id { get; set; }
-    public int? typ { get; set; }
-    public int? status { get; set; }
-    public int? act_status { get; set; }
+    public int? Status { get; set; }
+    public int? Debt_status { get; set; }
     public string fio_vz { get; set; }
-    public string name { get; set; }
     public string portfolio { get; set; }
     public string fio { get; set; }
     public DateTime? birth_date { get; set; }
     public string contract { get; set; }
-    public string exec_number { get; set; }
-    public decimal? total_sum { get; set; }
-    public string dsc { get; set; }
+    public string fssp_doc_num { get; set; }
     public string court_doc_num { get; set; }
-    public DateTime? court_date { get; set; }
+    public string fssp_name { get; set; }
+    public string fssp_address { get; set; }
     public string court_name { get; set; }
-    public string court_adress { get; set; }
+    public string court_address { get; set; }
+    public string exec_number { get; set; }
+    public string dsc { get; set; }
+    public string full_adr { get; set; }
     public decimal? start_sum { get; set; }
     public string number { get; set; }
     public string series { get; set; }
-    public string birth_place { get; set; }
-    public string full_adr { get; set; }
-    public DateTime? exec_date { get; set; }
     public string inn { get; set; }
+    public DateTime? exec_date { get; set; }
+    public string birth_place { get; set; }
 
 
 }
--- Utils/Searcher.cs
+++ Utils/Searcher.cs
@@ -4,6 +4,7 @@ using Microsoft.Office.Interop.Excel;
 using System.Collections.Generic;
 using System.Data.Odbc;
 using System.Linq;
+using System.Windows.Documents;
 using System.Windows.Forms;
 
 
@@ -35,41 +36,59 @@ namespace DocumentAdder.Utils
                 dataGridView1.Rows.Clear();
             if (dataGridView2.Rows.Count != 0)
                 dataGridView2.Rows.Clear();
-            string where = GetSQL("la.");
 
-            /*Settings.command = "select TOP(100) la.id,case la.typ when 0 then 'Неопределен' when 1 then 'Приказ' when 2 then 'Иск' when 3 then 'Правопреемство' when 4 then 'Банкротство' end typ," +
-                "case la.typ when 2 then (select name from dict where parent_id = 25 and code = la.act_status) else (select name from dict where parent_id = 18 and code = la.status) end status," +
-                " u.f+' '+u.i+' '+u.o fio_vz, la.name,pr.name,p.f+' '+p.i+' '+p.o,p.birth_date,la.contract, la.exec_number,	lc.name," +
-                "convert(varchar,la.total_sum) + ' Руб', la.dsc from law_act la left join person p on p.id = la.r_person_id " +
-                "left join law_court lc on lc.id=la.r_court_id 	left join portfolio pr on pr.id = la.r_portfolio_id left join work_task wt on wt.r_debt_id = la.r_debt_id left join users u on u.id = wt.r_user_id" +
-                $" where {where}";*/
-
-            var data = Program.db.LawAct.Where(
+            var data1 = Program.db.LawAct.Where(
+                x => EF.Functions.Like(x.Person.f + x.Person.i + x.Person.o, $"{_last_name}{_first_name}{_middle_name}%")
+                );
+            var data2 = Program.db.LawExec.Where(
                 x => EF.Functions.Like(x.Person.f + x.Person.i + x.Person.o, $"{_last_name}{_first_name}{_middle_name}%")
                 );
             if (_kd != "")
-                data = data.Where(x => EF.Functions.Like(x.Debt.contract, _kd + "%"));
+            {
+                data1 = data1.Where(x => EF.Functions.Like(x.Debt.contract, _kd + "%"));
+                data2 = data2.Where(x => EF.Functions.Like(x.Debt.contract, _kd + "%"));
+            }
             if (_id != "")
-                data = data.Where(x => EF.Functions.Like(x.id.ToString(), _id + "%"));
-            var subquery = Program.db.LawExec.GroupBy(x => x.r_act_id).Select(x => new { r_act_id = x.Key, court_doc_num = x.Max(a => a.court_doc_num), court_date = x.Max(a => a.court_date) });
-            var newData = data.Join(subquery, (x) => x.id, (x) => x.r_act_id, (x, y) => new { LawAct = x, LawExec = y });
-            var select = newData.Select(x => new LawActResult()
+            {
+                data1 = data1.Where(x => EF.Functions.Like(x.id.ToString(), _id + "%"));
+                data2 = data2.Where(x => EF.Functions.Like(x.id.ToString(), _id + "%"));
+            }
+            if (_exec_num != "")
+            {
+                data1 = data1.Where(x => EF.Functions.Like(x.exec_number, _exec_num + "%"));
+                data2 = data2.Where(x => EF.Functions.Like(x.LawAct.exec_number, _exec_num + "%"));
+            }
+            var subquery1 = data1.Join(Program.db.LawExec.GroupBy(x => x.r_act_id).
+                Select(x => new
+                {
+                    r_act_id = x.Key,
+                    court_doc_num = x.Max(a => a.court_doc_num),
+                    court_date = x.Max(a => a.court_date)
+                }),
+                (x) => x.id,
+                (x) => x.r_act_id,
+                (x, y) => new
+                {
+                    LawAct = x,
+                    LawExec = y
+                });
+            var select1 = subquery1.Select(x => new LawActResult()
             {
                 id = x.LawAct.id,
                 typ = x.LawAct.typ,
                 status = x.LawAct.status,
                 act_status = x.LawAct.act_status,
-                fio_vz = x.LawAct.User.f + " " + x.LawAct.User.i + " " + x.LawAct.User.o,
+                fio_vz = x.LawAct.Debt.WorkTask.User.f + " " + x.LawAct.Debt.WorkTask.User.i + " " + x.LawAct.Debt.WorkTask.User.o,
                 name = x.LawAct.name,
                 portfolio = x.LawAct.Portfolio.name,
                 fio = x.LawAct.Person.f + " " + x.LawAct.Person.i + " " + x.LawAct.Person.o,
                 birth_date = x.LawAct.Person.birth_date,
                 contract = x.LawAct.Debt.contract,
                 exec_number = x.LawAct.exec_number,
-                total_sum = x.LawAct.total_sum.ToString() + " Руб",
+                total_sum = x.LawAct.total_sum,
                 dsc = x.LawAct.dsc,
                 court_doc_num = x.LawExec.court_doc_num,
-                court_date = x.LawExec.court_date.Value.ToShortDateString(),
+                court_date = x.LawExec.court_date,
                 court_name = x.LawAct.LawCourt.name,
                 court_adress = x.LawAct.LawCourt.name,
                 start_sum = x.LawAct.Debt.start_sum,
@@ -80,55 +99,36 @@ namespace DocumentAdder.Utils
                 exec_date = x.LawAct.court_order_date,
                 inn = x.LawAct.Person.inn
             });
-            var result = select.ToList();
-
-            Settings.command = "select TOP(100) la.id,case la.typ when 0 then 'Неопределен' when 1 then 'Приказ' when 2 then 'Иск' when 3 then 'Правопреемство' when 4 then 'Банкротство' end typ, " +
-            "case when la.typ > 2 then (select name from dict where parent_id = 25 and code = la.act_status) else (select name from dict where parent_id = 18 and code = la.status) end status, " +
-            "u.f+' '+u.i+' '+u.o fio_vz, la.name, pr.name,p.f+' '+p.i+' '+p.o,p.birth_date,d.contract, la.exec_number, lc.name, " +
-            "convert(varchar,la.total_sum) + ' Руб', la.dsc, le.court_doc_num,CONVERT(varchar, le.court_date, 104) court_date,lc.name court_name, lc.address court_adress,cast(d.start_sum as decimal(16,2)) start_sum, " +
-            "pass.number,pass.series,p.birth_place, a.full_adr, la.court_order_date exec_date, p.inn from law_act la " +
-            "left join person p on p.id = la.r_person_id " +
-            "left join law_court lc on lc.id = la.r_court_id " +
-            "left join portfolio pr on pr.id = la.r_portfolio_id " +
-            "left join work_task wt on wt.r_debt_id = la.r_debt_id " +
-            "left join users u on u.id = wt.r_user_id " +
-            "left join (select r_act_id, max(court_doc_num) court_doc_num,max(court_date) court_date from law_exec GROUP BY r_act_id) le on le.r_act_id = la.id " +
-            "left join debt d on d.id = la.r_debt_id " +
-            "left join passport pass on pass.id=p.r_passport_id " +
-            "left join address a on a.parent_id = p.id and a.typ=1 " +
-            $"where {where}";
-            using (OdbcDataAdapter dataAdapter = new OdbcDataAdapter(Settings.command, Program.Conn))
+            var select2 = data2.Select(x => new LawExecResult()
             {
-                dataAdapter.Fill(dataSet1.DataTable1);
-            }
-            string where1 = GetSQL("le.");
-            /*Settings.command1 = "select TOP(100) le.id,(select name from dict where parent_id = 77 and code = le.state) Status,(select name from dict where parent_id = 6 and code=d.status) Debt_status," +
-                "u.f+' '+u.i+' '+u.o fio_vz,pr.name,p.f+' '+p.i+' '+p.o,p.birth_date,le.contract,le.fssp_doc_num , le.court_doc_num,lc.name,convert(varchar,la.total_sum) + ' Руб',lc1.name,la.exec_number," +
-                " le.dsc from law_exec le left join person p on p.id = le.r_person_id left join portfolio pr on pr.id = le.r_portfolio_id left join law_act la on la.id=le.r_act_id " +
-                "left join debt d on d.id = le.r_debt_id left join law_court lc on lc.id=le.r_court_id left join law_court lc1 on lc1.id = la.r_court_id left join work_task wt on wt.r_debt_id = la.r_debt_id" +
-                " left join users u on u.id = wt.r_user_id " +
-                $" where {where1} and le.state != 5 and le.state != 6";*/
-            Settings.command1 = "select TOP(100) le.id,(select name from dict where parent_id = 77 and code = le.state) Status,(select name from dict where parent_id = 6 and code=d.status) Debt_status, " +
-                "u.f+' '+u.i+' '+u.o fio_vz,pr.name,p.f+' '+p.i+' '+p.o,p.birth_date,d.contract,le.fssp_doc_num , le.court_doc_num,lc.name,convert(varchar,la.total_sum) + ' Руб',lc1.name,la.exec_number, " +
-                "le.dsc, CONVERT(varchar, le.court_date, 104) court_date1,lc1.address court_adress1,cast(d.start_sum as decimal(16,2)) start_sum1, pass.number number1,pass.series series1,p.birth_place birth_place1, " +
-                "a.full_adr full_adr1, la.court_order_date exec_date1, p.inn inn1 from law_exec le " +
-                "left join person p on p.id = le.r_person_id " +
-                "left join portfolio pr on pr.id = le.r_portfolio_id " +
-                "left join law_act la on la.id=le.r_act_id " +
-                "left join debt d on d.id = le.r_debt_id " +
-                "left join law_court lc on lc.id=le.r_court_id " +
-                "left join law_court lc1 on lc1.id = la.r_court_id " +
-                "left join work_task wt on wt.r_debt_id = la.r_debt_id " +
-                "left join users u on u.id = wt.r_user_id " +
-                "left join passport pass on pass.id=p.r_passport_id " +
-                "left join address a on a.parent_id = p.id and a.typ=1 " +
-                $" where {where1} and le.state != 5 and le.state != 6";
-            using (OdbcDataAdapter dataAd = new OdbcDataAdapter(Settings.command1, Program.Conn))
-            {
-                dataAd.Fill(dataSet1.DataTable2);
-            }
-            dataGridView1.DataSource = result;
-            dataGridView2.DataSource = dataSet1.DataTable2;
+                id = x.id,
+                Status = x.state,
+                Debt_status = x.Debt.status,
+                fio_vz = x.Debt.WorkTask.User.f + " " + x.Debt.WorkTask.User.i + " " + x.Debt.WorkTask.User.o,
+                portfolio = x.Portfolio.name,
+                fio = x.Person.f + " " + x.Person.i + " " + x.Person.o,
+                birth_date = x.Person.birth_date,
+                contract = x.Debt.contract,
+                fssp_doc_num = x.fssp_doc_num,
+                court_doc_num = x.court_doc_num,
+                fssp_name = x.LawCourt.name,
+                fssp_address = x.LawCourt.address,
+                court_name = x.LawAct.LawCourt.name,
+                court_address = x.LawAct.LawCourt.address,
+                exec_number = x.LawAct.exec_number,
+                dsc = x.dsc,
+                full_adr = x.Person.Addresses.Where(x => x.typ == 1).Select(x => x.full_adr).FirstOrDefault(),
+                start_sum = x.Debt.start_sum,
+                number = x.Person.Passport.number,
+                series = x.Person.Passport.series,
+                inn = x.LawAct.Person.inn,
+                exec_date = x.LawAct.court_order_date,
+                birth_place = x.Person.birth_place
+            });
+            var result1 = select1.ToList();
+            var result2 = select2.ToList();
+            dataGridView1.DataSource = result1;
+            dataGridView2.DataSource = result2;
         }
 
         private string GetSQL(string prefix)
