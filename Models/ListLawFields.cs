0b99c53cb37f379deeae7c49e06828d79421e756
Alex Dev Sm <leshasmlesha@gmail.com>
2023-09-26 11:37:44 +0300

    feat(model): написал конструктор

---
+++ Models/ListLawFields.cs
@@ -0,0 +1,123 @@
+﻿#nullable enable
+using DatabaseContact.DatabaseContact;
+using DatabaseContact.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocumentAdder.Models;
+
+public class ChangeData {
+    public int typ { get; init; }
+
+}
+
+public class ListValue<T>
+{
+    public delegate void LawData(T data, DataModel value, SettingsModel settings);
+    public delegate bool LawCondition(T data, DataModel value, SettingsModel settings);
+
+    public string? database_order { get; init; }
+    public string? database_act { get; init; }
+    public string? instance { get; init; }
+    public object? value { get; init; }
+    public LawData? change;
+    public LawCondition? condition { get; init; }
+}
+public class ListLawFields
+{
+    public List<ListValue<LawAct>> LawAct = new()
+    {
+        new() { database_order = "exec_number", instance = "Exec_number" },
+        new() { database_order = "status", database_act = "act_status", instance = "Status" },
+        new() { database_order = "dsc", instance = "Dsc" },
+        new() { database_order = "court_order_date", instance = "Court_order_date" },
+        new() { database_order = "court_exec_date", instance = "Court_exec_date" },
+        new() { database_order = "receipt_date", instance = "Receipt_date", database_act = "receipt_dt" },
+        new()
+        {
+            database_order = "correct_period_date",
+            instance = "Correct_period_date",
+        },
+        new()
+        {
+            instance = "Return_date",
+            change = (data, value, _) =>
+            {
+                data.LawActProtokols.Add(new LawActProtokol()
+                {
+                    typ = 72,
+                    r_user_id = Settings.user_id,
+                    dt = DateTime.Now,
+                    dsc = $"Дата возврата приказа: {value.Return_date?.ToShortDateString()} Комментарий: {value.Dsc}"
+                });
+            }
+        },
+        new()
+        {
+            instance = "Cancel_date",
+            change = (data, value, _) =>
+            {
+                data.LawActProtokols.Add(new ()
+                {
+                    typ = 8,
+                    r_user_id = Settings.user_id,
+                    dt = DateTime.Now,
+                    dsc = $"Дата отмены приказа: {value.Cancel_date?.ToShortDateString()} Комментарий: {value.Dsc}"
+                });
+            }
+        },
+        new()
+        {
+            instance = "Session_date",
+            change = (data, value, _) =>
+            {
+                data.LawSessions.Add(new()
+                { dt = value.Session_date, status = 1 });
+            }
+        },
+        new()
+        {
+            database_order = "court_order_delivery",
+            value = 2,
+        },
+        new(){ 
+            database_order = "int_color",
+            condition = (_, _, settings) => {
+                return settings.color != -1;
+            },
+            change = (data, value, settings) => 
+            {
+                data.int_color = settings.color;
+                data.LawActProtokols.Add(new() {
+                    typ = 2,
+                    dt=DateTime.Now,
+                    r_user_id=Settings.user_id,
+                    dsc = $"Изменен цвет: {settings.color} Пользователем: {Settings.username}"
+                });
+            }
+        }
+    };
+    public List<ListValue<LawExec>> LawExec = new()
+    {
+        new() { database_order = "exec_number", instance = "Exec_number" },
+        new() { database_order = "state", instance = "Status" },
+        new() { database_order = "fssp_doc_num", instance = "Fssp_doc_num" },
+        new() { database_order = "court_doc_num", instance = "Court_doc_num" },
+        new() { database_order = "dsc", instance = "Dsc" },
+        new() { database_order = "court_date", instance = "Court_order_date" },
+        new() { database_order = "entry_force_dt", instance = "Court_exec_date" },
+        new() { database_order = "start_date", instance = "Start_date" },
+        new() { database_order = "finish_date", instance = "Finish_date" },
+        new() { database_order = "restriction_to_leave_dt", instance = "Restriction_to_leave_dt" },
+        new() { database_order = "reject_date", instance = "Reject_date" },
+        new() { database_order = "receipt_act_dt", value = DateTime.Now }
+    };
+
+    public void writeChanges(i_collectContext db) {
+        db.ChangeTracker.DetectChanges();
+        db.ChangeTracker.Entries();
+    }
+}
--- Utils/Data.cs
+++ Utils/Data.cs
@@ -5,9 +5,11 @@ using DocumentAdder.Models;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Linq;
+using System.Reflection;
 using System.Windows.Forms;
 
 namespace DocumentAdder.Utils;
+
 
 class Data
 {
@@ -18,6 +20,34 @@ class Data
     public static int? status { get { return _status; } set { _status = value; } }
     public static string id { get { return _id; } set { _id = value; } }
 
+    private static void callerData<T>(T row, DataModel data, SettingsModel enabled, ListValue<T> item, bool act)
+    {
+
+        MethodInfo setter = null;
+        if (item.database_order != null)
+            setter = row.GetType().GetMethod("set_" + item.database_order);
+        if (act && item.database_act != null)
+            setter = row.GetType().GetMethod("set_" + item.database_act);
+
+        var checker = enabled.GetType().GetMethod("get_" + item.instance);
+        if ((bool)checker.Invoke(enabled, null) && (item.condition == null || (item.condition != null && item.condition(row, data, enabled))))
+        {
+            if (item.value != null)
+            {
+                setter.Invoke(row, new[] { item.value });
+            }
+            if (item.change != null)
+            {
+                item.change(row, data, enabled);
+            }
+            if (setter != null)
+            {
+                var getter = data.GetType().GetMethod("get_" + item.instance);
+                setter.Invoke(row, new[] { getter.Invoke(data, null) });
+            }
+        }
+    }
+
 
     public static i_collectContext Update(i_collectContext db, DataModel data, SettingsModel enabled)
     {
@@ -27,7 +57,11 @@ class Data
         }
         var LawExec = db.LawExec.Include(x => x.LawAct).FirstOrDefault(x => x.id == id_int);
         var LawAct = db.LawAct.FirstOrDefault(x => x.id == id_int);
-
+        var fields = new ListLawFields();
+        foreach (var item in fields.LawAct)
+        {
+            callerData(LawAct, data, enabled, item, LawAct.typ > 1);
+        }
         if (enabled.exec_number)
         {
             if (data.Typ == LawTyp.LawExec)
