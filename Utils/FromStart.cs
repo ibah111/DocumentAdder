e0ee0c792536289f28dd6c640f749ee679999a53
Alex Dev Sm <leshasmlesha@gmail.com>
2023-10-25 09:36:35 +0300

    fix(download): исправил получение пол-лей

--- Utils/FromStart.cs
+++ Utils/FromStart.cs
@@ -41,9 +41,6 @@ internal class FromStart
     internal static async Task GetUserDB()
     {
         using var db = await Program.factory_db.CreateDbContextAsync();
-        foreach (var item in await db.User.Where(x => string.IsNullOrEmpty(x.i)).Select(x => new { x.id, x.f, x.i }).ToListAsync())
-        {
-            Settings.users.Add(item.id, $"{item.f} {item.i}");
-        }
+        Settings.users = (await db.User.Where(x => string.IsNullOrEmpty(x.i)).Select(x => new { x.id, x.f, x.i }).ToListAsync()).ToDictionary(x => x.id, item => $"{item.f} {item.i}");
     }
 }
