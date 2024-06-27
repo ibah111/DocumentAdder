de7fc1a0210235830f92950b13cca96a5b943386
Ibah <starlord77@mail.ru>
2024-06-27 17:19:09 +0300

    feat: second select after empty results in person

--- Utils/Searcher.cs
+++ Utils/Searcher.cs
@@ -53,11 +53,32 @@ public class Searcher
             if (!string.IsNullOrEmpty(fio.f))
             {
                 data1 = data1.Where(
-                    x => EF.Functions.Like(x.Person.f, $"{fio.f}%") || EF.Functions.Like(x.Debt.DebtGuarantor.fio, $"{fio}")
+                    x => EF.Functions.Like(x.Person.f, $"{fio.f}%")
                     );
                 data2 = data2.Where(
-                    x => EF.Functions.Like(x.Person.f, $"{fio.f}%" ) || EF.Functions.Like(x.Debt.DebtGuarantor.fio, $"{fio}")
+                    x => EF.Functions.Like(x.Person.f, $"{fio.f}%" )
                     );
+
+                if(data1.Count() == 0)
+                {
+                    /*
+                     */
+                    data1 = data1
+                        .Where(
+                            x => EF.Functions.Like(x.Debt.DebtGuarantor.fio, $"{fio.f}%")
+                        );
+                }
+                if(data1.Count() == 0)
+                {
+                    /*
+                     */
+                    data2 = data2
+                        .Where(
+                            x => EF.Functions.Like(x.Debt.DebtGuarantor.fio, $"${fio.f}%")
+                        );
+                }
+
+
             }
             if (!string.IsNullOrEmpty(fio.i))
             {
